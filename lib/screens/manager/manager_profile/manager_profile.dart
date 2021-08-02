09ced2df93170b9a24b71b6fefdd2d943f62dc84
Akash Giri <akashgiri27aug@gmail.com>
2021-08-02 12:38:25 +0530

    Manager profile UI added.

--- lib/screens/manager/manager_profile/manager_profile.dart
+++ lib/screens/manager/manager_profile/manager_profile.dart
@@ -1,3 +1,8 @@
+import 'package:fastcheque/screens/common/change_password/change_password.dart';
+import 'package:fastcheque/screens/common/upload_signature/upload_signature.dart';
+import 'package:fastcheque/utils/color.dart';
+import 'package:fastcheque/utils/constants.dart';
+import 'package:fastcheque/widgets/error_information.dart';
 import 'package:flutter/material.dart';
 
 class ManagerProfile extends StatefulWidget {
@@ -10,6 +15,85 @@ class ManagerProfile extends StatefulWidget {
 class _ManagerProfileState extends State<ManagerProfile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      padding: EdgeInsets.all(defaultPadding * 2),
+      children: [
+        Text(
+          'Hi, Name!',
+          style: Theme.of(context).textTheme.headline4?.copyWith(
+                color: primaryColor,
+              ),
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.zero,
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.keyboard_arrow_down_outlined,
+              color: primaryColor,
+            ),
+          ),
+          title: Text(
+            'Store name,',
+            style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                  color: primaryColor,
+                ),
+          ),
+          subtitle: Text(
+            'Address line 1',
+            style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                  color: primaryColor,
+                ),
+          ),
+        ),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        ErrorInformation(
+          hint: [
+            'Upload you signature',
+            'Reset your password',
+          ],
+        ),
+        SizedBox(
+          height: defaultPadding * 2,
+        ),
+        Text(
+          'Settings',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text('Upload signature'),
+          leading: Icon(Icons.edit_outlined),
+          trailing: Icon(Icons.keyboard_arrow_right_outlined),
+          onTap: () => Navigator.of(context)
+              .pushNamed(UploadSignature.UPLOAD_SIGNATURE_ROUTE),
+        ),
+        Divider(
+          color: hintColor,
+          indent: 50,
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text('Change password'),
+          leading: Icon(Icons.lock_outline),
+          trailing: Icon(Icons.keyboard_arrow_right_outlined),
+          onTap: () => Navigator.of(context)
+              .pushNamed(ChangePassword.CHANGE_PASSWORD_ROUTE),
+        ),
+        Divider(
+          color: hintColor,
+          indent: 50,
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Text('Sign out'),
+          leading: Icon(Icons.logout),
+          trailing: Icon(Icons.keyboard_arrow_right_outlined),
+          onTap: () => null,
+        )
+      ],
+    );
   }
 }
