9c1cfdcc286e45a757ae9477f30b43a2e13ff101
Akash Giri <akashgiri27aug@gmail.com>
2021-08-04 22:48:46 +0530

    Staff profile data binding complete

--- lib/screens/staff/staff_profile/staff_profile.dart
+++ lib/screens/staff/staff_profile/staff_profile.dart
@@ -1,9 +1,16 @@
+import 'package:fastcheque/model/staff_model.dart';
+import 'package:fastcheque/model/store_model.dart';
 import 'package:fastcheque/screens/common/change_password/change_password.dart';
+import 'package:fastcheque/screens/common/login/login_screen.dart';
 import 'package:fastcheque/screens/common/upload_signature/upload_signature.dart';
+import 'package:fastcheque/service/authentication_service.dart';
 import 'package:fastcheque/utils/color.dart';
 import 'package:fastcheque/utils/constants.dart';
+import 'package:fastcheque/utils/preference_key.dart';
 import 'package:fastcheque/widgets/error_information.dart';
 import 'package:flutter/material.dart';
+
+import '../../../main.dart';
 
 class StaffProfile extends StatefulWidget {
   const StaffProfile({Key? key}) : super(key: key);
@@ -13,25 +20,44 @@ class StaffProfile extends StatefulWidget {
 }
 
 class _StaffProfileState extends State<StaffProfile> {
+  late StaffModel _staffModel;
+  late StoreModel _currentStore;
+
+  loadStaffFromCache() {
+    setState(() {
+      _staffModel =
+          StaffModel.fromJson(prefs.getString(PreferenceKey.USER_DATA)!);
+      _currentStore =
+          StoreModel.fromJson(prefs.getString(PreferenceKey.CURRENT_STORE)!);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadStaffFromCache();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       padding: EdgeInsets.all(defaultPadding * 2),
       children: [
         Text(
-          'Hi, Name!',
+          'Hi, ${_staffModel.name}!',
           style: Theme.of(context).textTheme.headline4?.copyWith(
                 color: primaryColor,
               ),
         ),
         Text(
-          'Store name,',
+          '${_currentStore.businessName}',
           style: Theme.of(context).textTheme.subtitle1?.copyWith(
                 color: primaryColor,
               ),
         ),
         Text(
-          'Address line 1',
+          '${_currentStore.businessAddress}',
           style: Theme.of(context).textTheme.subtitle1?.copyWith(
                 color: primaryColor,
               ),
@@ -40,10 +66,7 @@ class _StaffProfileState extends State<StaffProfile> {
           height: defaultPadding,
         ),
         ErrorInformation(
-          hint: [
-            'Upload you signature',
-            'Reset your password',
-          ],
+          hint: getErrorList(),
         ),
         SizedBox(
           height: defaultPadding * 2,
@@ -81,9 +104,25 @@ class _StaffProfileState extends State<StaffProfile> {
           title: Text('Sign out'),
           leading: Icon(Icons.logout),
           trailing: Icon(Icons.keyboard_arrow_right_outlined),
-          onTap: () => null,
+          onTap: () {
+            AuthenticationService.instance.logoutUser();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil(Login.LOGIN_ROUTE, (route) => false);
+          },
         )
       ],
     );
+  }
+
+  getErrorList() {
+    List<String> err = [];
+
+    if (_staffModel.signatureUrl.isEmpty) err.add('Upload you signature');
+    if (_staffModel.isPasswordTemporary)
+      err.add(
+        'Reset your password',
+      );
+
+    return err;
   }
 }
